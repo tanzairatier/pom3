97aa32e0297da27c8dae5dc2c7556e6d6530009e
kralljoe <kralljoe@gmail.com>
2017-04-27 17:16:44 -0700

    more stuff

---
+++ pom3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pom3',
+  packages = ['pom3'],
+  version = '1.0',
+  description = 'Simulation of agile requirements engineering.',
+  author = 'Joseph Krall',
+  author_email = 'kralljoe@gmail.com',
+  url = 'https://github.com/tanzairatier/pom3', 
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['agile', 'requirements', 'engineering', 'simulation', 'jmoo'], # arbitrary keywords
+  classifiers = [],
+)
